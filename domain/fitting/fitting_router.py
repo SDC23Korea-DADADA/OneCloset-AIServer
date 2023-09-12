e092238de23c15745d3eaaf7b2bf89bc7202df36
정수정 <worend365@gmail.com>
2023-09-12 17:23:55 +0900

    feat: todo 작성

--- domain/fitting/fitting_router.py
+++ domain/fitting/fitting_router.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter, File, UploadFile
-from fastapi.responses import FileResponse
+from domain.s3.s3_service import upload_file
 
 router = APIRouter(
     prefix="/fast/fitting",
 )
 
 @router.post("/")
-async def create_virtual_fitting(images: list[UploadFile]):
-    # 모델 이미지에 상의, 하의를 피팅한 이미지를 반환하는 로직
-    return {"filenames " : [image.filename for image in images]}
+async def create_virtual_fitting(model, upperwear, underwear, onepiece):
+    # TODO: vton 모델을 통해 사람 이미지에 상의, 하의 또는 한벌옷을 피팅한 이미지 생성
+    print(model, upperwear, underwear, onepiece)
+
+    # s3에 가상 피팅 이미지 업로드 후 반환
+    # url = upload_file(image)
+    url = "https://fitsta-bucket.s3.ap-northeast-2.amazonaws.com/123.png"
+    return {"image " : url}
 
