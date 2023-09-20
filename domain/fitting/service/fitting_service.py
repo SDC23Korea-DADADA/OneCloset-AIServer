29498c704dfd8c9f99083323c31172049a6cc872
ChanHwan <song960705@naver.com>
2023-09-21 00:25:35 +0900

    feat: virtual fitting preprocess API 구현

---
+++ domain/fitting/service/fitting_service.py
@@ -0,0 +1,41 @@
+import uuid
+import subprocess
+import glob
+import os
+import logging
+
+# s3를 사용하기 위해 import
+from domain.s3.s3_service import upload_general_file
+
+logger = logging.getLogger(__name__)
+
+
+def preprocess(url):
+    preprocess_path = "~/preprocess/"
+    dataroot = "~/data"
+    fname = uuid.uuid4()[:13]
+    logger.info("[Preprocess] fname(uuid): " + fname)
+
+    exit_code = subprocess.call(
+        ["bash", preprocess_path + "preprocess.sh", "--dataroot", dataroot, "--dir", fname, "--url", url])
+
+    if exit_code == 1:
+        logger.error("[Preprocess] preprocess failed")
+        raise Exception("preprocess failed")
+
+    file_map = {
+        "2.json": "keypoint",
+        "4.png": "labelMap",
+        "5.jpg": "skeleton",
+        "5.png": "dense",
+        "uv.npz": "denseNpz"
+    }
+
+    url_dict = {}
+    for file in glob.glob('*', root_dir=dataroot + "/" + fname):
+        postfix = file.split("_")[-1]
+        aws_url = upload_general_file(file)
+
+        url_dict[file_map[postfix]] = aws_url
+
+    return url_dict
--- domain/s3/s3_service.py
+++ domain/s3/s3_service.py
@@ -8,15 +8,28 @@ def upload_file(image):
     identifier = str(uuid.uuid4())
     filename = identifier + image.filename
     s3.upload_fileobj(image.file, AWS_S3_BUCKET_NAME, filename,
-                      ExtraArgs={'ContentType': image.content_type} # 이거 없으면 url로 접근 시 파일 다운로드
+                      ExtraArgs={'ContentType': image.content_type}  # 이거 없으면 url로 접근 시 파일 다운로드
                       )
     return AWS_S3_URL + filename
+
+
+def upload_image(image):
+    s3.upload_fileobj(image.file, AWS_S3_BUCKET_NAME, image.filename,
+                      ExtraArgs={'ContentType': image.content_type}  # 이거 없으면 url로 접근 시 파일 다운로드
+                      )
+    return AWS_S3_URL + image.filename
+
+
+def upload_general_file(file):
+    s3.upload_fileobj(file.file, AWS_S3_BUCKET_NAME, file.filename)
+    return AWS_S3_URL + file.filename
+
 
 def upload_file_Image(image, filename):
     # S3에 업로드
     identifier = str(base64.urlsafe_b64encode(uuid.uuid4().bytes))
     upload_filename = identifier + filename
     s3.put_object(Bucket=AWS_S3_BUCKET_NAME, Key=upload_filename, Body=image.getvalue(),
-                         ContentType='image/png')
+                  ContentType='image/png')
 
     return AWS_S3_URL + upload_filename
