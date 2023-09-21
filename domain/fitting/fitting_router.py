b796f3cc7f57ae97b8efe53e5ad1f583329b7aed
ChanHwan <song960705@naver.com>
2023-09-22 01:16:36 +0900

    feat: 가상피팅 처리시간 출력

--- domain/fitting/fitting_router.py
+++ domain/fitting/fitting_router.py
@@ -1,13 +1,11 @@
+import time
 from typing import List, Optional
-
 from fastapi import APIRouter, File, UploadFile
-
 from domain.fitting.schemas.ModelRegistResponse import ModelRegistResponse
 from domain.fitting.service.face_quality_service import increase_face_quality
 from domain.s3.s3_service import upload_file
 from pydantic import BaseModel
 from domain.fitting.service.fitting_service import preprocess, fitting
-
 from domain.fitting.schemas.FittingRequestModel import FittingRequestModel
 
 
@@ -22,6 +20,8 @@ router = APIRouter(
 
 @router.post("/")
 async def create_virtual_fitting(request: Optional[FittingRequestModel] = None):
+    start_time = time.time()
+
     model_url = request.model
     clothes_list = request.clothesList
     result_url = None
@@ -31,9 +31,18 @@ async def create_virtual_fitting(request: Optional[FittingRequestModel] = None):
 
     high_quality_url = increase_face_quality(request, model_url)
 
+    end_time = time.time()
+    print("가상피팅 처리시간 :", end_time - start_time, "seconds")
+
     return {"image": high_quality_url}
 
 
 @router.post("/preprocess", response_model=ModelRegistResponse)
 async def regist_fitting_model(request: ImageModel):
-    return preprocess(request.image)
+    start_time = time.time()
+
+    result = preprocess(request.image)
+
+    end_time = time.time()
+    print("Model 전처리 처리시간 :", end_time - start_time, "seconds")
+    return result
